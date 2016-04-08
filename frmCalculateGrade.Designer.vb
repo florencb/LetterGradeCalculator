1b89839625905a1fda0dd0c9194f8edfd97d6834
florencb <bradjflorence@gmail.com>
2016-04-08 12:48:13 -0400

    initial commit

---
+++ frmCalculateGrade.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCalculateGrade
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtNumberGrade = New System.Windows.Forms.TextBox()
+        Me.txtLetterGrade = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(34, 141)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 2
+        Me.btnCalculate.Text = "&Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(158, 141)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "&Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(34, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Number Grade:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(34, 84)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Letter Grade:"
+        '
+        'txtNumberGrade
+        '
+        Me.txtNumberGrade.Location = New System.Drawing.Point(133, 41)
+        Me.txtNumberGrade.Name = "txtNumberGrade"
+        Me.txtNumberGrade.Size = New System.Drawing.Size(100, 20)
+        Me.txtNumberGrade.TabIndex = 0
+        '
+        'txtLetterGrade
+        '
+        Me.txtLetterGrade.Location = New System.Drawing.Point(133, 80)
+        Me.txtLetterGrade.Name = "txtLetterGrade"
+        Me.txtLetterGrade.ReadOnly = True
+        Me.txtLetterGrade.Size = New System.Drawing.Size(100, 20)
+        Me.txtLetterGrade.TabIndex = 1
+        Me.txtLetterGrade.TabStop = False
+        '
+        'frmCalculateGrade
+        '
+        Me.AcceptButton = Me.btnCalculate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(273, 201)
+        Me.Controls.Add(Me.txtLetterGrade)
+        Me.Controls.Add(Me.txtNumberGrade)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Name = "frmCalculateGrade"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Calculate Letter Grade"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtNumberGrade As System.Windows.Forms.TextBox
+    Friend WithEvents txtLetterGrade As System.Windows.Forms.TextBox
+
+End Class
---
+++ frmCalculateGrade.vb
@@ -0,0 +1,51 @@
+﻿Public Class frmCalculateGrade
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    'this is the button calculate event listener
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        'creates variable that will hold letter grade
+        'converts the data type to a decimal data type
+        Dim numberGrade As Decimal = CDec(txtNumberGrade.Text)
+
+        'declares and initializes a variable that will hold the letter grade 
+        Dim letterGrade As String = " "
+
+        'takes number grade input and assigns it the appropiate letter
+        If numberGrade >= 88 Then
+            letterGrade = "A"
+        ElseIf numberGrade >= 80 AndAlso numberGrade <= 87 Then
+            letterGrade = "B"
+        ElseIf numberGrade >= 68 AndAlso numberGrade <= 79 Then
+            letterGrade = "C"
+        ElseIf numberGrade >= 60 AndAlso numberGrade <= 67 Then
+            letterGrade = "D"
+        Else
+            letterGrade = "F"
+        End If
+
+        ' displays letter grade in the letter grade text box
+        txtLetterGrade.Text = letterGrade
+
+        ' moves focus back to number grade text box
+        txtNumberGrade.Select()
+    End Sub
+
+    Private Sub txtNumberGrade_TextChanged(sender As Object, e As EventArgs) Handles txtNumberGrade.TextChanged
+       
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' closes when exit is clicked
+        Me.Close()
+    End Sub
+
+    Private Sub EsleIf(p1 As Boolean)
+        Throw New NotImplementedException
+    End Sub
+
+End Class
